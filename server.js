7bf51b976b902cb5024384e60cf28460052bce97
testingdeploy <widiyatnoteguh@gmail.com>
2024-08-26 16:04:54 +0700

    Add index.html and update root route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
+const path = require('path');
 
 const app = express();
 const port = 3001;
@@ -32,7 +33,7 @@ try {
   });
 
   app.get('/', (req, res) => {
-    res.send('Welcome to the Stability AI Web App');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 
   app.post('/generate-image', async (req, res) => {
